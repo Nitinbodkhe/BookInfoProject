557b7c59e3169f327340cab47678e94a75f5ce8f
Nitin Bodkhe <nitinbodkhe07@gmail.com>
2025-08-14 17:22:39 +0530

    Initial commit : Set up BookInfoProject with Book class

---
+++ src/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    public static void main(String[] args) {
+
+        // Declaring the book details
+        String title = "Hidden Hindu";
+        String authorName = "Akshat Gupta";
+        int publicationYear = 2024;
+
+        // Printing the details
+        System.out.println("** Here is the book details:");
+        System.out.println();
+        System.out.println("Book Name : " + title);
+        System.out.println("Author Name : " + authorName);
+        System.out.println("Publication Year : " + publicationYear);
+    }
+}
